35a77ef9ce7fa078f5324ac1e0254f7c88effd93
Tom Theile <tom.theile@hnw-deutschland-de>
2017-05-12 13:24:09 +0200

    now not working - steps per days as line graph

--- src/pages/read-db/read-db.ts
+++ src/pages/read-db/read-db.ts
@@ -4,8 +4,8 @@ import { SQLite } from '@ionic-native/sqlite';
 import { Toast } from '@ionic-native/toast';
 import { File } from '@ionic-native/file';
 
-import { AngularEchartsModule } from 'angular2-echarts';
-declare var echarts: any;
+import * as moment from 'moment';
+
 /**
  * Generated class for the ReadDB page.
  *
@@ -19,11 +19,17 @@ declare var echarts: any;
 export class ReadDB {
 
   db;
+  chartOption = {};
 
   testresult1 = '..';
   testresult2 = '..';
 
+  dateStart = '2017-02-01';
+  dateEnd = '2017-03-01';
+  
+
   constructor(private file: File, public navCtrl: NavController, private sqlite: SQLite,private toast: Toast) {
+    this.setCahrt1();
   }
 
   ionViewDidLoad() {
@@ -84,7 +90,7 @@ group by timestamp/(3600*24)`;
     let rowCount='rc:';
     this.db.transaction((transaction) => {
       transaction.executeSql(sqltext, [], (tx, results) => {
-        let len = results.rows.length, i;
+        let len = results.rows.length;
         rowCount+=(len);
         for (let i = 0; i < len; i++){
           console.log('item i:', results.rows.item(i));
@@ -106,91 +112,171 @@ group by timestamp/(3600*24)`;
 
 
 
-  readDBandgiveBackTable(sqlText:string){
+  readDBandgiveBackTable(sqlText:string, sqlParameters, columns=['value1']){
     //input: complete SQL
     //output: data = [[row],[row]] or ....
+    let data = [];
 
-      this.db.executeSql(sqlText, {}).then((result) => {
+    return this.db.executeSql(sqlText, sqlParameters).then((result) => {
+      console.log('gab-readDB - executed! results are here', JSON.stringify(result));
 
-        console.log('gab-readDB - executed! results are here', JSON.stringify(result));
-        this.testresult1 = JSON.stringify(result);
-        this.testresult2 = '|';
-        for (var row of result.rows.items) {
-          console.log(row); //of: items of the array; in: indexes of the array
-          //this.testresult2 += row.datum + ' : ' + row.daysteps + ' || '
-        }
-
-        for(let x = 0; x < result.rows.length; x++) {
-            console.log("Daysteps: " + result.rows.item(x).daysteps +
-                ", datum: " + result.rows.item(x).acctNo);
-            this.testresult2 += "Daysteps: " + result.rows.item(x).daysteps + ", datum: " + result.rows.item(x).datum;
-        }
-
-      }, (err) => {
-        console.error('Unable to execute sql: ', err);
-        this.dberror(err);
-      });
+      for(let x = 0; x < result.rows.length; x++) {
+          console.log("row: ", result.rows.item(x));
+          let row = [];
+          for(let col of columns){
+            console.log(col, columns);
+            row.push(result.rows.item(x)[col]);
+          }
+          data.push(row);//result.rows.item(x)
+      }
+      return data;
+    }, (err) => {
+      console.error('Unable to execute sql: ', err);
+      this.dberror(err);
+    });
 
   }
 
+  showSteptsPerDay(){
+    let sqlText = `select SUM(STEPS) as daysteps, datetime(ROUND(AVG(timestamp)), 'unixepoch') as datum, timestamp
+from MI_BAND_ACTIVITY_SAMPLE
+where (timestamp between strftime('%s','now','-:start days') and strftime('%s','now','-:end days'))
+group by timestamp/(3600*24);`
 
+    let startDays = moment().diff(moment(this.dateStart),'days');
+    let endDays = moment().diff(moment(this.dateEnd),'days');
+    console.log(startDays, endDays);
 
-  chartOption = {
-    title: {
-      text: 'chart-test'
-    },
-    tooltip : {
-      trigger: 'axis'
-    },
-    legend: {
-      data:['Montag','Dienstag','Mittwoch','Thursday','Friday','Saturday','Sunday']
-    },
-    toolbox: {
-      feature: {
-        saveAsImage: {}
-      }
-    },
-    grid: {
-      left: '3%',
-      right: '4%',
-      bottom: '3%',
-      containLabel: true
-    },
-    xAxis : [
-      {
-        type : 'category',
-        boundaryGap : false,
-        data : ['low','middle','high']
-      }
-    ],
-    yAxis : [
-      {
-        type : 'value'
-      }
-    ],
-    series : [
-      {
-        name:'einse',
-        type:'line',
-        stack: 'jey',
-        areaStyle: {normal: {}},
-        data:[120, 132, 101, 134, 90, 230, 210]
+    let parameters = [startDays.toString(),endDays.toString(), '22'];
+    let parameters2 = {'start':startDays.toString(),'end':endDays.toString()};
+    let columns = ['datum','daysteps'];
+
+    let data = this.readDBandgiveBackTable(sqlText, parameters2, columns).then(res=>{
+      console.log('results:')
+      console.log(res)
+      let seriesData1 = res.map(x=> x[1]);
+      let seriesxAxis= res.map(x=> x[0]);
+      console.log(seriesData1)
+      console.log(seriesxAxis)
+      this.showSimpleChart1(seriesxAxis,seriesData1);
+    });
+  }
+
+  showSimpleChart1(seriesxAxis,seriesData1){
+    
+    this.chartOption = {
+      title: {
+        text: 'Test Chart'
       },
-      {
-        name:'zweise',
-        type:'line',
-        stack: 'jey',
-        areaStyle: {normal: {}},
-        data:[120, 232, 51, 134, 90, 230, 210]
+      tooltip : {
+        trigger: 'axis'
       },
-      {
-        name:'dreise',
-        type:'line',
-        stack: 'jey',
-        areaStyle: {normal: {}},
-        data:[120, 132, 101, 134, 90, 230, 210]
-      }      
-    ]
+      dataZoom: {
+          show: true,
+          start : 60,
+          end : 100
+      },
+      legend: {
+        data:['Steps per day']
+      },
+      grid: {
+        left: '3%',
+        right: '4%',
+        bottom: '3%',
+        containLabel: true
+      },
+      xAxis : [
+        {
+          type : 'category',
+          boundaryGap : false,
+        data:seriesxAxis
+        }
+      ],
+      yAxis : [
+        {
+          type : 'value'
+        }
+      ],
+      series : [
+        {
+          name:'Steps per day',
+          type:'line',
+          stack: 'jey',
+          areaStyle: {normal: {}},
+          data:seriesData1
+        }
+      ]
+    };
+
+  }
+
+  setCahrt1() {
+
+    this.chartOption = {
+      title: {
+        text: 'Test Chart'
+      },
+      tooltip : {
+        trigger: 'axis'
+      },
+      dataZoom: {
+          show: true,
+          start : 30,
+          end : 70
+      },
+      legend: {
+        data:['einse','zweise','dreise','vierse']
+      },
+      grid: {
+        left: '3%',
+        right: '4%',
+        bottom: '3%',
+        containLabel: true
+      },
+      xAxis : [
+        {
+          type : 'category',
+          boundaryGap : false,
+        data:['Monday','Tuesday','Wednesday','Thursday','Friday','Saturday','Sunday']
+        }
+      ],
+      yAxis : [
+        {
+          type : 'value'
+        }
+      ],
+      series : [
+        {
+          name:'einse',
+          type:'line',
+          stack: 'jey',
+          areaStyle: {normal: {}},
+          data:[10, 32, 41, 54, 66, 77, 88]
+        },
+        {
+          name:'zweise',
+          type:'line',
+          stack: 'jey',
+          areaStyle: {normal: {}},
+          data:[130, 110, 111, 80, 70, 50, 20]
+        },
+        {
+          name:'dreise',
+          type:'line',
+          stack: 'zwo',
+          areaStyle: {normal: {}},
+          data:[5, 30, 50, 66, 44, 20, 2]
+        },
+        {
+          name:'vierse',
+          type:'line',
+          stack: 'zwo',
+          areaStyle: {normal: {}},
+          data:[15,15,15,15,15,15,30]
+        }      
+      ]
+    };
+
   }
 
 
